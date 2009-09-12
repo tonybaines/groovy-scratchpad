b4128bdf82e8eb0d359b175bdb528b108e5db2a6
Tony <tony@neuromancer>
2009-09-12 13:49:43 +0100

    savepoint

---
+++ src/test/groovy/AlsoSimpleStory.groovy
@@ -0,0 +1,11 @@
+scenario "Simple story", {
+	given "A string", {
+		s = "Hello"
+	}
+	when "it is made upper case", {
+		S = s.toUpperCase()
+	}
+	then "it should be in upper case", {
+		S.shouldBe "HELLO"
+	}
+}
