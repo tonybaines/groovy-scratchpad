570dcd6f47c4cbcce40f470c1632bc65102d5068
tony <tony@count-zero>
2009-09-14 22:08:20 +0100

    savepoint

---
+++ src/main/groovy/tmp/GLogger.groovy
@@ -0,0 +1,35 @@
+package tmp
+import groovy.lang.Delegate;
+import java.util.logging.Level
+import java.util.logging.Logger;
+
+
+public class GLogger {
+	@Delegate Logger logger
+	public static GLogger getLogger(Class c) {
+		getLogger(c.name)
+	}
+	public static GLogger getLogger(String name) {
+        new GLogger(Logger.getLogger(name))
+    }
+	
+	private GLogger(delegate) {
+		this.logger = delegate
+	}
+
+	// Expose proper log methods which accept exceptions
+	def methodMissing(String name, args) {
+		Level level = Level.parse(name.toUpperCase())
+		switch (args.length) {
+        case 1: 
+			this.logger.log(level, args[0]) 
+        	break
+        case 2:
+			this.logger.log(level, args[0], args[1])
+			break
+		default: 
+			throw new MissingMethodException(name, args)
+		}
+	}
+
+}
